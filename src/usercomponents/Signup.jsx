6032c1a97af265e0c7427a654a86adee9532c2cb
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-08-15 13:18:27 +0530

    Login Page & Sign Up Page Created

--- src/usercomponents/Signup.jsx
+++ src/usercomponents/Signup.jsx
@@ -1,5 +1,66 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div>
+      <div class="bg-grey-lighter min-h-screen flex flex-col">
+        <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+          <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+            <h1 class="mb-8 text-3xl text-center">Sign Up</h1>
+            <input
+              type="email"
+              class="block border border-grey-light w-full p-3 rounded mb-4"
+              name="email"
+              placeholder="Email"
+              //   onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <input
+              type="password"
+              class="block border border-grey-light w-full p-3 rounded mb-4"
+              name="password"
+              placeholder="Password"
+              //   onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <Button
+              type="submit"
+              //   onClick={logInUser}
+              style={{ backgroundColor: "#fb923c" }}
+              class="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+            >
+              Create Account
+            </Button>
+            <hr />
+
+            <p className="text-center">or</p>
+
+            <hr />
+
+            <Button
+              type="submit"
+              //   onClick={googleSignIn}
+              style={{ backgroundColor: "#4285F4" }}
+              class="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+            >
+              Sign Up With Google
+            </Button>
+          </div>
+
+          <div class="text-grey-dark mt-6">
+            Alredy have an account ?
+            <Link
+              to={"/login"}
+              class="ml-2 no-underline border-b border-blue text-blue"
+            >
+              Log In
+            </Link>
+            .
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
