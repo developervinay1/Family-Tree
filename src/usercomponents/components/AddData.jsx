35c81c3f4c013367b636e73aa6d6584cc19aff33
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-08-17 10:54:03 +0530

    Bug Fixed

--- src/usercomponents/components/AddData.jsx
+++ src/usercomponents/components/AddData.jsx
@@ -1,17 +1,52 @@
-import { doc, setDoc } from "firebase/firestore";
+// import { doc, setDoc } from "firebase/firestore";
+// import React, { useRef } from "react";
+// import { db } from "../../../firebase";
+
+// export default function AddData({ path }) {
+//   const name = useRef();
+//   const handleForm = async (e) => {
+//     e.preventDefault();
+//     const docRef = doc(db, path, name.current.value);
+//     await setDoc(docRef, { Name: name.current.value });
+//     // Reset
+
+//     e.target.reset();
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={handleForm}>
+//         <input placeholder="Enter Name" type="text" ref={name} />
+//         <button type="submit">Add</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+import { doc, setDoc, collection, addDoc } from "firebase/firestore";
 import React, { useRef } from "react";
 import { db } from "../../../firebase";
 
 export default function AddData({ path }) {
   const name = useRef();
+
   const handleForm = async (e) => {
     e.preventDefault();
-    const docRef = doc(db, path, name.current.value);
-    await setDoc(docRef, { Name: name.current.value });
-    // Reset
 
+    const parentDocRef = doc(db, path, name.current.value);
+
+    // Set data in the parent document
+    await setDoc(parentDocRef, { Name: name.current.value });
+
+    // Create a subcollection reference named "Child"
+    const childCollectionRef = collection(parentDocRef, "Childs");
+
+    // Add a document to the "Child" subcollection
+    await addDoc(childCollectionRef, { ChildProperty: "Value" });
+
+    // Reset the form
     e.target.reset();
   };
+
   return (
     <div>
       <form onSubmit={handleForm}>
